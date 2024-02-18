2030dc598d62e42c97c5d5d06869c79221beef6f
Chamika Chiran <chamikacp3@gmail.com>
2024-02-18 20:15:00 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,6 @@
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+export default function App() {
+  return <ViewImageScreen />;
+}
